974834240444267c6103034da94051634396befb
morvenhuang <morven.huang@gmail.com>
2025-09-09 10:28:15 +0800

    misc

--- src/test/java/com/github/morvenhuang/leetcodejourney/_0033m_SearchInRotatedSortedArray.java
+++ src/test/java/com/github/morvenhuang/leetcodejourney/_0033m_SearchInRotatedSortedArray.java
@@ -4,19 +4,19 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 /**
- * Suppose an array sorted in ascending order is rotated（旋转） at some pivot（轴） unknown to you beforehand.
- * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
- * You are given a target value to search. If found in the array return its index, otherwise return -1.
- * You may assume no duplicate exists in the array.
- * Your algorithm’s runtime complexity must be in the order of O(log n).
- * <p>
- * Example 1:
- * Input: nums = [4,5,6,7,0,1,2], target = 0
- * Output: 4
- * <p>
- * Example 2:
- * Input: nums = [4,5,6,7,0,1,2], target = 3
- * Output: -1
+ * Suppose an array sorted in ascending order is rotated（旋转） at some pivot（轴） unknown to you beforehand. <br>
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]). <br>
+ * You are given a target value to search. If found in the array return its index, otherwise return -1. <br>
+ * You may assume no duplicate exists in the array. <br>
+ * Your algorithm’s runtime complexity must be in the order of O(log n). <br>
+ * <br>
+ * Example 1: <br>
+ * Input: nums = [4,5,6,7,0,1,2], target = 0 <br>
+ * Output: 4 <br>
+ * <br>
+ * Example 2: <br>
+ * Input: nums = [4,5,6,7,0,1,2], target = 3 <br>
+ * Output: -1 <br>
  */
 public class _0033m_SearchInRotatedSortedArray {
 
